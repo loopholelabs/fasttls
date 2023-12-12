249e9286c9498ccd717306ba5ae1762248d68d38
Shivansh Vij <shivanshvij@loopholelabs.io>
2023-12-11 19:16:03 -0800

    Adding libc-based ktls setup

--- src/crypto.rs
+++ src/crypto.rs
@@ -15,9 +15,41 @@
 */
 
 use std::error::Error;
+use std::marker::PhantomData;
 use std::mem::size_of;
 use rustls::ConnectionTrafficSecrets;
 use crate::constants;
+
+#[repr(C)]
+#[derive(Default, Clone)]
+pub struct __IncompleteArrayField<T>(PhantomData<T>, [T; 0]);
+impl<T> __IncompleteArrayField<T> {
+    #[inline]
+    pub const fn new() -> Self {
+        __IncompleteArrayField(PhantomData, [])
+    }
+    #[inline]
+    pub fn as_ptr(&self) -> *const T {
+        self as *const _ as *const T
+    }
+    #[inline]
+    pub fn as_mut_ptr(&mut self) -> *mut T {
+        self as *mut _ as *mut T
+    }
+    #[inline]
+    pub unsafe fn as_slice(&self, len: usize) -> &[T] {
+        std::slice::from_raw_parts(self.as_ptr(), len)
+    }
+    #[inline]
+    pub unsafe fn as_mut_slice(&mut self, len: usize) -> &mut [T] {
+        std::slice::from_raw_parts_mut(self.as_mut_ptr(), len)
+    }
+}
+impl<T> std::fmt::Debug for __IncompleteArrayField<T> {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fmt.write_str("__IncompleteArrayField")
+    }
+}
 
 #[repr(C)]
 #[derive(Debug, Clone)]
@@ -28,44 +60,112 @@ pub struct Info {
 
 #[repr(C)]
 #[derive(Debug, Clone)]
-pub struct Secret {
+pub struct TLS12SM4GCM {
     pub info: Info,
-    pub iv: [u8; 12usize],
+    pub iv: [u8; 8usize],
+    pub key: [u8; 16usize],
+    pub salt: [u8; 4usize],
+    pub rec_seq: [u8; 8usize],
+}
+
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub struct TLS12SM4CCM {
+    pub info: Info,
+    pub iv: [u8; 8usize],
+    pub key: [u8; 16usize],
+    pub salt: [u8; 4usize],
+    pub rec_seq: [u8; 8usize],
+}
+
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub struct TLS12AESGCM128 {
+    pub info: Info,
+    pub iv: [u8; 8usize],
+    pub key: [u8; 16usize],
+    pub salt: [u8; 4usize],
+    pub rec_seq: [u8; 8usize],
+}
+
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub struct TLS12AESGCM256 {
+    pub info: Info,
+    pub iv: [u8; 8usize],
     pub key: [u8; 32usize],
     pub salt: [u8; 4usize],
     pub rec_seq: [u8; 8usize],
 }
 
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub struct TLS12AESCCM128 {
+    pub info: Info,
+    pub iv: [u8; 8usize],
+    pub key: [u8; 16usize],
+    pub salt: [u8; 4usize],
+    pub rec_seq: [u8; 8usize],
+}
+
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub struct TLS12Chacha20Poly1305 {
+    pub info: Info,
+    pub iv: [u8; 12usize],
+    pub key: [u8; 32usize],
+    pub salt: __IncompleteArrayField<u8>,
+    pub rec_seq: [u8; 8usize],
+}
+
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub enum Secret {
+    AESGCM128(TLS12AESGCM128),
+    AESGCM256(TLS12AESGCM256),
+    AESCCM128(TLS12AESCCM128),
+    Chacha20Poly1305(TLS12Chacha20Poly1305),
+    SM4GCM(TLS12SM4GCM),
+    SM4CCM(TLS12SM4CCM),
+}
+
 impl Secret {
-    pub fn as_ptr(&self) -> *const libc::c_void{
-        self as *const _ as *const libc::c_void
+    pub(crate) fn as_ptr(&self) -> *const libc::c_void {
+        match self {
+            Secret::AESGCM128(info) => info as *const _ as *const libc::c_void,
+            Secret::AESGCM256(info) => info as *const _ as *const libc::c_void,
+            Secret::AESCCM128(info) => info as *const _ as *const libc::c_void,
+            Secret::Chacha20Poly1305(info) => info as *const _ as *const libc::c_void,
+            Secret::SM4GCM(info) => info as *const _ as *const libc::c_void,
+            Secret::SM4CCM(info) => info as *const _ as *const libc::c_void,
+        }
     }
-    pub fn size(&self) -> u32 {
-        size_of::<Secret>() as u32
+
+    pub(crate) fn size(&self) -> usize {
+        match self {
+            Secret::AESGCM128(_) => size_of::<TLS12AESGCM128>(),
+            Secret::AESGCM256(_) => size_of::<TLS12AESGCM256>(),
+            Secret::AESCCM128(_) => size_of::<TLS12AESCCM128>(),
+            Secret::Chacha20Poly1305(_) => size_of::<TLS12Chacha20Poly1305>(),
+            Secret::SM4GCM(_) => size_of::<TLS12SM4GCM>(),
+            Secret::SM4CCM(_) => size_of::<TLS12SM4CCM>(),
+        }
     }
-}
-
-fn offset_iv_12(iv: &rustls::crypto::cipher::Iv) -> [u8; 12usize] {
-    [iv.as_ref()[4], iv.as_ref()[5], iv.as_ref()[6], iv.as_ref()[7], iv.as_ref()[8], iv.as_ref()[9], iv.as_ref()[10], iv.as_ref()[11], 0, 0, 0, 0]
-}
-
-fn offset_iv_32(key: &rustls::crypto::cipher::AeadKey) -> [u8; 32usize] {
-    [key.as_ref()[0], key.as_ref()[1], key.as_ref()[2], key.as_ref()[3], key.as_ref()[4], key.as_ref()[5], key.as_ref()[6], key.as_ref()[7], key.as_ref()[8], key.as_ref()[9], key.as_ref()[10], key.as_ref()[11], key.as_ref()[12], key.as_ref()[13], key.as_ref()[14], key.as_ref()[15], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 }
 
 pub(crate) fn convert_to_secret(tls_version: u16, seq: u64, secrets: ConnectionTrafficSecrets) -> Result<Secret, Box<dyn Error>> {
     Ok(match secrets {
         ConnectionTrafficSecrets::Aes128Gcm { key, iv } => {
-            let mut corrected_iv = [0u8; 12usize];
+            let mut corrected_iv = [0u8; 8usize];
             for i in 0..8 {
                 corrected_iv[i] = iv.as_ref()[i+4];
             }
             let salt = (&iv.as_ref()[..4]).try_into().map_err(|_| "invalid iv length for AES128GCM Salt")?;
-            let mut corrected_key = [0u8; 32usize];
+            let mut corrected_key = [0u8; 16usize];
             for i in 0..16 {
                 corrected_key[i] = key.as_ref()[i];
             }
-            Secret {
+            Secret::AESGCM128(TLS12AESGCM128 {
                 info: Info {
                     version: tls_version,
                     cipher_type: constants::TLS_CIPHER_AES_GCM_128 as _,
@@ -74,16 +174,16 @@ pub(crate) fn convert_to_secret(tls_version: u16, seq: u64, secrets: ConnectionT
                 salt,
                 key: corrected_key,
                 rec_seq: seq.to_be_bytes(),
-            }
+            })
         },
         ConnectionTrafficSecrets::Aes256Gcm { key, iv } => {
-            let mut corrected_iv = [0u8; 12usize];
+            let mut corrected_iv = [0u8; 8usize];
             for i in 0..12 {
                 corrected_iv[i] = iv.as_ref()[i+4];
             }
             let salt = (&iv.as_ref()[..4]).try_into().map_err(|_| "invalid iv length for AES256GCM Salt")?;
             let corrected_key = (&key.as_ref()[..32]).try_into().map_err(|_| "invalid key length for AES256GCM Key")?;
-            Secret {
+            Secret::AESGCM256(TLS12AESGCM256 {
                 info: Info {
                     version: tls_version,
                     cipher_type: constants::TLS_CIPHER_AES_GCM_256 as _,
@@ -92,13 +192,13 @@ pub(crate) fn convert_to_secret(tls_version: u16, seq: u64, secrets: ConnectionT
                 salt,
                 key: corrected_key,
                 rec_seq: seq.to_be_bytes(),
-            }
+            })
         },
         ConnectionTrafficSecrets::Chacha20Poly1305 { key, iv } => {
             let corrected_iv = (&iv.as_ref()[..12]).try_into().map_err(|_| "invalid iv length for Chacha20Poly1305 IV")?;
-            let salt = [0u8; 4];
+            let salt = __IncompleteArrayField::new();
             let corrected_key = (&key.as_ref()[..32]).try_into().map_err(|_| "invalid key length for Chacha20Poly1305 Key")?;
-            Secret {
+            Secret::Chacha20Poly1305(TLS12Chacha20Poly1305 {
                 info: Info {
                     version: tls_version,
                     cipher_type: constants::TLS_CIPHER_CHACHA20_POLY1305 as _,
@@ -107,7 +207,7 @@ pub(crate) fn convert_to_secret(tls_version: u16, seq: u64, secrets: ConnectionT
                 salt,
                 key: corrected_key,
                 rec_seq: seq.to_be_bytes(),
-            }
+            })
         },
         _ => {
             return Err("unsupported cipher suite".into());
--- src/lib.rs
+++ src/lib.rs
@@ -60,9 +60,7 @@ pub struct HandshakeResult {
 #[derive(Debug, Clone)]
 pub struct HandshakeSecrets {
     pub rx: crypto::Secret,
-    pub rx_size: u32,
     pub tx: crypto::Secret,
-    pub tx_size: u32,
 }
 
 pub fn server_session(config: &ServerConfig) -> Result<ServerConnection, Box<dyn Error>> {
@@ -119,9 +117,7 @@ pub fn server_secrets(session: Box<ServerConnection>) -> Result<HandshakeSecrets
     let tx_crypto_secret = crypto::convert_to_secret(tls_version, tx_seq, tx_secrets)?;
 
     Ok(HandshakeSecrets {
-        rx_size: rx_crypto_secret.size(),
         rx: rx_crypto_secret,
-        tx_size: tx_crypto_secret.size(),
         tx: tx_crypto_secret,
     })
 }
